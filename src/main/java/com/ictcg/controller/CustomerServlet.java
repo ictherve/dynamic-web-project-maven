0eee615760db4e27334eeb0a0b97441e58e1d569
EkemKingni <herve.ekemkingni@ictcg.com>
2019-05-30 13:07:32 +0200

    Display all the students in the database

--- src/main/java/com/ictcg/controller/CustomerServlet.java
+++ src/main/java/com/ictcg/controller/CustomerServlet.java
@@ -1,41 +1,40 @@
 package com.ictcg.controller;
 
 import java.io.IOException;
+import java.util.Collection;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class CustomerServlet
- */
+import com.ictcg.dataaccess.CustomerRepository;
+import com.ictcg.model.Customer;
+
 @WebServlet("/CustomerServlet")
 public class CustomerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public CustomerServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		this.findAll(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// doGet(request, response);
+	}
+	
+private void findAll(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Collection<Customer> customers = CustomerRepository.getInstance().findAll();
+		System.out.println("All cuqtomers : " + customers);
+		request.setAttribute("customers", customers);
+		this.getServletContext().getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
 }
